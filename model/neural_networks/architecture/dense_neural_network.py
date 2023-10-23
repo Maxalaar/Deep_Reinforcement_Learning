0ace0c6ed7de7c083d5988414b45528769c3826e
Maxalaar <maxalaar@protonmail.com>
2023-10-23 10:54:02 +0200

    MOD : dense_neural_network.py implementing the capability for multiple layers

--- model/neural_networks/architecture/dense_neural_network.py
+++ model/neural_networks/architecture/dense_neural_network.py
@@ -2,50 +2,27 @@ import torch
 import torch.nn as nn
 
 
-# class Regressor(pl.LightningModule):
-#     def __init__(self, input_size, output_size, n_layers, n_neurons):
-#         super().__init__()
-#         self.input_size = input_size
-#         self.output_size = output_size
-#
-#         layers = []
-#         for i in range(n_layers):
-#             in_features = input_size if i == 0 else n_neurons
-#             out_features = n_neurons
-#             layers.append(nn.Linear(in_features, out_features))
-#             layers.append(nn.ReLU())
-#
-#         self.hidden_layers = nn.Sequential(*layers)
-#         self.output_layer = nn.Linear(n_neurons, output_size)
-#
-#     def forward(self, x):
-#         x = self.hidden_layers(x)
-#         x = self.output_layer(x)
-#         return x
-#
-#     def training_step(self, batch, batch_idx):
-#         x, y = batch
-#         y_predicts = self(x)
-#         loss = nn.MSELoss()(y_predicts, y)
-#         self.log('train_loss', loss)
-#         return loss
-#
-#     def configure_optimizers(self):
-#         optimizer = optim.SGD(self.parameters(), lr=1e-2)
-#         return optimizer
-
 class DenseNeuralNetwork(nn.Module):
     def __init__(self, number_layer, layer_size, *args, **kwargs):
         super(DenseNeuralNetwork, self).__init__()
         self._number_layer = number_layer
         self._layer_size = layer_size
 
+        self.hidden_layers = None
+        self.output_layer = None
+
     def create(self, input_size, output_size):
-        self.fc1 = nn.Linear(input_size, self._layer_size)
-        self.fc2 = nn.Linear(self._layer_size, output_size)
+        layers = []
+        for i in range(self._number_layer):
+            in_features = input_size if i == 0 else self._layer_size
+            out_features = self._layer_size
+            layers.append(nn.Linear(in_features, out_features))
+            layers.append(nn.LeakyReLU(0.1))
+
+        self.hidden_layers = nn.Sequential(*layers)
+        self.output_layer = nn.Linear(self._layer_size, output_size)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        # x = torch.sigmoid(self.fc2(x))
-        x = self.fc2(x)
+        x = self.hidden_layers(x)
+        x = self.output_layer(x)
         return x
